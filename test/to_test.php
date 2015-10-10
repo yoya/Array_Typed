e5f2a8a9d1f997886fd386dfaff76d762a3ec22d
Yoshihiro Yamazaki <yoya@awm.jp>
2015-10-10 20:31:17 +0900

    toArray のテスト

---
+++ test/to_test.php
@@ -0,0 +1,26 @@
+<?php
+
+if (is_readable('vendor/autoload.php')) {
+    require 'vendor/autoload.php';
+} else {
+    require_once 'Array/Uint8.php';
+    require_once 'Array/Sint8.php';
+    require_once 'Array/Uint16.php';
+    require_once 'Array/Sint16.php';
+}
+
+$a = array_pad([], 100, 0);
+$a[99] = 99;
+$a[98] = -98;
+
+$arr = Array_Uint8::fromArray($a);
+var_dump($arr->toArray()[99]);
+
+$arr = Array_Sint8::fromArray($a);
+var_dump($arr->toArray()[98]);
+
+$arr = Array_Uint16::fromArray($a);
+var_dump($arr->toArray()[99]);
+
+$arr = Array_Sint16::fromArray($a);
+var_dump($arr->toArray()[98]);
