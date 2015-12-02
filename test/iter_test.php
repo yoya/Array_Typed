4df30157b32dc70b0f1abbc001aecef448048706
USAMI Kenta <tadsan@zonu.me>
2015-12-02 22:52:34 +0900

    php-cs-fixer fix --fixers=short_array_syntax --level=symfony -- ./test

--- test/iter_test.php
+++ test/iter_test.php
@@ -6,23 +6,22 @@ if (is_readable('vendor/autoload.php')) {
     require_once 'Array/Sint8.php';
 }
 
-echo "Array_Sint8(0)".PHP_EOL;
+echo 'Array_Sint8(0)'.PHP_EOL;
 $arr = new Array_Sint8(0);
 foreach ($arr as $e) {
     var_dump($e);
 }
-echo "Array_Sint8(7)".PHP_EOL;
+echo 'Array_Sint8(7)'.PHP_EOL;
 $arr = new Array_Sint8(7);
 foreach ($arr as $e) {
     var_dump($e);
 }
 
-
 $a = array_pad([], 10, 0);
 $a[0] = -1;
 $a[8] = -8;
 $a[9] = 9;
-echo "Array_Sint8::fromArray".PHP_EOL;
+echo 'Array_Sint8::fromArray'.PHP_EOL;
 $arr = Array_Sint8::fromArray($a);
 foreach ($arr as $e) {
     var_dump($e);
--- test/type_test.php
+++ test/type_test.php
@@ -9,7 +9,6 @@ if (is_readable('vendor/autoload.php')) {
     require_once 'Array/Sint16.php';
 }
 
-
 $arr = new Array_Uint8(100);
 $arr[99] = 99;
 var_dump($arr[99]);
@@ -21,7 +20,6 @@ var_dump($arr[99]);
 $arr = new Array_Uint16(100);
 $arr[99] = 999;
 var_dump($arr[99]);
-
 
 $arr = new Array_Sint16(100);
 $arr[99] = -999;
